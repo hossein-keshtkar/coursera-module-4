b41d0c74620f0ac5e4e10aecb09c915f4bb2d0ee
hossein.k <keshtkarhosdevelop2@hotmail.com>
2023-08-31 21:41:11 -0400

    formHandler is not defined error

--- src/index.mjs
+++ src/index.mjs
@@ -3,24 +3,36 @@ import { listItemGenerator } from "./funcs/listItemGenerator.mjs";
 import { names } from "./data/names.mjs";
 
 const ul = document.getElementsByTagName("ul")[0];
-const firstChild = ul.firstChild;
+document.addEventListener("DOMContentLoaded", () => {
+  const firstChild = ul.firstChild;
 
-const greetToNames = () => {
-  const listItems = greetToArr(names);
+  const greetToNames = () => {
+    const listItems = greetToArr(names);
 
-  listItems.forEach((item) => {
-    firstChild ? ul.insertBefore(item, firstChild) : ul.appendChild(item);
-  });
-};
+    listItems.forEach((item) => {
+      firstChild ? ul.insertBefore(item, firstChild) : ul.appendChild(item);
+    });
+  };
 
-const formHandler = (e) => {
-  e.preventDefault();
-  const userInput = e && e.target[0].value;
-  const li = listItemGenerator(userInput);
+  const formHandler = (e) => {
+    if (!e) return;
 
-  firstChild ? ul.insertBefore(li, firstChild) : ul.append(li);
-};
+    e.preventDefault();
+    const userInput = e.target[0].value;
+    const btn = document.getElementById("exampleModal");
 
-const greetBtn = document.getElementById("greet-names");
-greetBtn.addEventListener("click", greetToNames);
-document.addEventListener("submit", formHandler);
+    if (userInput === "" || /^[\d]/.test(userInput)) {
+      btn.setAttribute("data-bs-toggle", "modal");
+      btn.setAttribute("data-bs-target", "#exampleModal");
+    } else {
+      btn.removeAttribute("data-bs-toggle");
+      btn.removeAttribute("data-bs-target");
+      const li = listItemGenerator(userInput);
+      firstChild ? ul.insertBefore(li, firstChild) : ul.append(li);
+    }
+  };
+
+  const greetBtn = document.getElementById("greet-names");
+  greetBtn.addEventListener("click", greetToNames);
+  document.addEventListener("submit", formHandler);
+});
